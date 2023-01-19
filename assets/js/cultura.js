96edf4c01ac21616229c1b75734f32f3d79ec1b1
Francisco Hormazabal <hormazabal.multimedia@gmail.com>
2023-01-19 18:34:19 -0300

    correciones reu

--- assets/js/cultura.js
+++ assets/js/cultura.js
@@ -251,8 +251,8 @@ $('.casoss').slick({
   centerMode: false,
   //focusOnSelect: true,
   variableWidth: true,
-  prevArrow: "<div class='page left bg-[#003A5C] flex flex-col cursor-pointer items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] left-[40%]'><i class='icon-arrow-left-page'></i></div>",
-  nextArrow: "<div class='page right bg-[#003A5C] flex flex-col cursor-pointer items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] right-[40%]'><i class='icon-arrow-right-page'></i></div>",
+  prevArrow: "<div class='page left bg-[#003A5C] flex flex-col cursor-pointer items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] left-[50%] ml-[-17%]'><i class='icon-arrow-left-page'></i></div>",
+  nextArrow: "<div class='page right bg-[#003A5C] flex flex-col cursor-pointer items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] right-[50%] mr-[-17%]'><i class='icon-arrow-right-page'></i></div>",
   responsive: [
     {
       breakpoint: 768,
@@ -275,8 +275,8 @@ $('.slideBe').slick({
   slidesToScroll: 1,
   centerMode: true,
   variableWidth: true,
-  prevArrow: "<div class='page left bg-[#003A5C] flex flex-col items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] left-[100px]'><i class='icon-arrow-left-page'></i></div>",
-  nextArrow: "<div class='page right bg-[#003A5C] flex flex-col items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] right-[100px]'><i class='icon-arrow-right-page'></i></div>",
+  prevArrow: "<div class='page left bg-[#003A5C] flex flex-col items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] left-[50%] ml-[-17%]'><i class='icon-arrow-left-page'></i></div>",
+  nextArrow: "<div class='page right bg-[#003A5C] flex flex-col items-center justify-center text-[#ffffff] w-[40px] h-[40px] rounded-full absolute bottom-[-75px] right-[50%] mr-[-17%]'><i class='icon-arrow-right-page'></i></div>",
 
 })
 var $vt = $("ul.vid-btn-tabs li"),
